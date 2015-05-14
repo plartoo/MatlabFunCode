2838ea9f870aedf184b2065295b024edf3d303f2
plartoo <pthiha@AH-PTHIHA.dhcp.mathworks.com>
2015-05-14 16:37:38 -0400

    Added explanation of the profiling results
    Explained that 'pause()' is taking most of the program's runtime, and
    that we could probably not need to redraw parts that aren't changing
    (e.g., face, eyeball, labels, etc.)

--- animate_clock.m
+++ animate_clock.m
@@ -7,20 +7,31 @@
 % To run,
 % >> animate_clock
 % 
-% Note: this script is dependent on the file below:
+% Note 1: this script is dependent on the file below:
 % - 'draw_clock.m'
+%
+% Note 2: If we set the hours of the animation to full day,
+% (that is, from 0:23 hours) the animation becomes choppy
+% after a few hours. I profiled the code and found that most 
+% of the runtime is spent in 'pause()' call. However, I do
+% believe that we could shave off some (small amount
+% compared to the time spend in 'pause()') time by not
+% re-drawing the whole componenets of the face. In other words,
+% we could redraw only the iris and the smile part of the face 
+% instead of the whole face. This is an afterthought and is 
+% a lesson learned for future MATLAB animation coding exercises.
 
 clearvars;  % start from fresh
 close all;
 
 profile on
-hours = 10:14;
+hours = 11:13;
 minutes = 0:59;
 
 for i = 1:numel(hours)
     for j = 1:numel(minutes)
         draw_clock(hours(i), minutes(j));
-        pause(0.001);
+        pause(0.05);
         refresh;
     end
 end
